186b363cde7a8af6fbcdf20603e08e7d574ac308
Leonardo Lopes <leonardosilvalls1908@gmail.com>
2025-12-26 12:35:27 -0300

    domain updated https

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://agroshop.app.br/api/v1';
+const API_BASE_URL = 'https://agroshop.app.br/api/v1';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
